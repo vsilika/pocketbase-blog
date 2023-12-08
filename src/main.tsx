76eee5f6fc54562d8170792e6de871466bf52917
hesoyam <sasa.lumenspei@gmail.com>
2023-12-08 23:46:23 +0100

    Add React Query and React Query Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import App from './App.tsx';
+import './index.css';
 
-import './index.css'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+    <ReactQueryDevtools  initialIsOpen={false} buttonPosition='bottom-left'/>
+  </QueryClientProvider>
 )
